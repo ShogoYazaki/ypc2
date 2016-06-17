c78fef03c25ae587002efc48d04841e465327a4d
ShogoYazaki <yazaki.s@gmail.com>
2016-06-17 07:27:48 +0000

    next user model

---
+++ db/migrate/20160617071423_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email,              null: false, default: ""
+      t.string :password_digest,    null: false, default: ""
+      t.integer :admin_level, :default => 1
+      t.integer :record_limit, :default => 30
+      
+      t.timestamps null: false
+    end
+
+      add_index :users, :email,                unique: true
+  end
+end
